a955919b1dcba4263613e6fedafd96fc618ee86e
Amdadul-haq <amdadulhaq3746@gmail.com>
2025-02-25 04:55:15 +0600

    edit donation model

--- models/Donation.js
+++ models/Donation.js
@@ -4,7 +4,7 @@ const donationSchema = new mongoose.Schema({
     donorName: { type: String, required: true },
     donationType: { type: String, enum: ['Regular', 'Mosuhimu Collection', 'Eid Collection', 'Others'], required: true },
     donationMonth: { type: String, required: true },
-    amount: { type: Number, required: true },
+    amount: { type: String, required: true },
     paymentMethod: { type: String, enum: ['Bkash', 'Nagad', 'Rocket', 'ByHand'], required: true },
     date: { type: Date, default: Date.now }
 });
